7adb9a89d2ce37b3e52f2b112a2d2282b8a53126
RafaelM4gn <rfl.magno02@gmail.com>
2022-10-11 01:19:30 -0300

    implements axios to use githubAPI in Projects page

--- pages/projects.vue
+++ pages/projects.vue
@@ -7,7 +7,7 @@
     >
       <template>
         <v-col
-          v-for="(item, i) in items"
+          v-for="(item, i) in repos"
           :key="i"
           cols="12"
           md="4"
@@ -19,7 +19,7 @@
               :class="{ 'on-hover': hover }"
             >
               <v-img
-                :src="item.img"
+                :src="img"
                 height="225px"
               >
                 <v-card-title class="text-h6 white--text">
@@ -28,19 +28,21 @@
                     justify="space-between"
                   >
                     <p class="mt-4 subheading text-left">
-                      {{ item.title }}
+                      {{ item.name }}
                     </p>
 
                     <div>
                       <p class="ma-0 text-body-1 font-weight-bold font-italic text-left">
-                        {{ item.text }}
+                        {{ item.description }}
                       </p>
                       <p class="text-caption font-weight-medium font-italic text-left">
-                        {{ item.subtext }}
+                        <!-- {{ item.subtext }} -->
                       </p>
                     </div>
                     <v-card-actions>
                       <v-btn
+                        :href="item.html_url"
+                        target="_blank"
                         :class="{ 'show-btns': hover }"
                         color="teal"
                         rounded
@@ -61,9 +63,12 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data: () => ({
     icons: ['mdi-rewind', 'mdi-play', 'mdi-fast-forward'],
+    img: 'https://images.unsplash.com/photo-1542320868-f4d80389e1c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3750&q=80',
     items: [
       {
         title: 'Projeto 1',
@@ -82,22 +87,16 @@ export default {
         text: 'subtitulo',
         subtext: 'Loren ipsum dolor no sit amet',
         img: 'https://images.unsplash.com/photo-1542320868-f4d80389e1c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3750&q=80'
-      },
-      {
-        title: 'Projeto 3',
-        text: 'subtitulo',
-        subtext: 'Loren ipsum dolor no sit amet',
-        img: 'https://images.unsplash.com/photo-1542320868-f4d80389e1c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3750&q=80'
-      },
-      {
-        title: 'Projeto 3',
-        text: 'subtitulo',
-        subtext: 'Loren ipsum dolor no sit amet',
-        img: 'https://images.unsplash.com/photo-1542320868-f4d80389e1c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3750&q=80'
       }
     ],
-    transparent: 'rgba(255, 255, 255, 0)'
-  })
+    transparent: 'rgba(255, 255, 255, 0)',
+    repos: null
+  }),
+  created: function () {
+    axios.get('https://api.github.com/users/RafaelM4gn/repos').then((response) => {
+      this.repos = response.data
+    })
+  }
 }
 </script>
 
